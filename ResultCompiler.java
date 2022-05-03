a902a07dc2846becb4a6f3bc508c38466a9ff9c6
Pietukor <104776333+Pietukor@users.noreply.github.com>
2022-05-03 23:26:57 +0300

    Update and rename Masiina.java to ResultCompiler.java

---
+++ ResultCompiler.java
@@ -0,0 +1,39 @@
+package com.example.harkkaduuni;
+
+import java.util.ArrayList;
+
+public class ResultCompiler {
+
+    //Singleton:
+
+    private static final ResultCompiler M = new ResultCompiler();
+    private static WindParser Vehe;
+    private static WaterParser Vehe2;
+
+    public static ResultCompiler getInstance() {
+        return M;
+    }
+
+    private ResultCompiler(){
+
+    }
+
+    //This method fetches information via WindParser and WaterParser and compiles the result as an ArrayListiksi, which it returns.
+    //This method also handles some errors caused by improper inputs.
+
+    public static ArrayList Haku(){
+        ArrayList tulos = new ArrayList<>();
+        ArrayList tuulitulos = new ArrayList<>();
+        String location = Fragment1.location;
+        Vehe = WindParser.getInstance();
+
+        tuulitulos = WindParser.parseWind();
+        try {
+        tulos.add(tuulitulos.get(0));
+        tulos.add(WaterParser.parseWater());
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("location not found");
+        }
+        return tulos;
+    }
+}
