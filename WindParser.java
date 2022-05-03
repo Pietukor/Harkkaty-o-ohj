05fa61b78ce2a79b3c6cc7a2f7a04769cf57c35f
Pietukor <104776333+Pietukor@users.noreply.github.com>
2022-05-03 23:27:31 +0300

    Update and rename TuuliParserVehje.java to WindParser.java

--- WindParser.java
+++ WindParser.java
@@ -2,8 +2,6 @@ package com.example.harkkaduuni;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import java.io.IOException;
@@ -13,30 +11,30 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
-public class TuuliParserVehje {
+public class WindParser {
 
     //Singleton:
 
-    private static final TuuliParserVehje P = new TuuliParserVehje();
+    private static final WindParser P = new WindParser();
 
-    public static TuuliParserVehje getInstance() {
+    public static WindParser getInstance() {
         return P;
     }
 
-    private TuuliParserVehje(){
+    private WindParser(){
 
     }
 
-    // Tämä metodi Hakee käyttäjän syöttämän paikkakunnan ja senhetkisen kellonajan mukaan ilmatieteen avoimesta rajapinnasta säädataa.
-    // metodi käsittelee xml-muotoisen dokumentin ja palauttaa String-muotoisen tuulennopeuden.
+    // This method fetches weather data based on current time and location given by user.
+    // This method parses the data and returns it as a string.
 
     public static ArrayList parseWind(){
         ArrayList tuotos = new ArrayList<>();
-        String sijainti = Fragment1.sijainti;
-        String paiva = Fragment1.paiva;
-        String aika = Fragment1.aika;
+        String location = Fragment1.location;
+        String date = Fragment1.date;
+        String time = Fragment1.time;
 
-            String url_tuuli = "https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::forecast::hirlam::surface::point::simple&place=" + sijainti + "&parameters=WindSpeedMS&timeStep=60&starttime=" + paiva + "T" + aika + "Z&endtime=" + paiva + "T" + aika + "Z";
+            String url_tuuli = "https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::forecast::hirlam::surface::point::simple&place=" + location + "&parameters=WindSpeedMS&timeStep=60&starttime=" + date + "T" + time + "Z&endtime=" + date + "T" + time + "Z";
 
         try {
 
