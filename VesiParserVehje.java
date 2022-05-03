559e6bd3692f4ff5ac761af0ec06c99b35e8ab4a
Pietukor <104776333+Pietukor@users.noreply.github.com>
2022-05-03 16:49:13 +0300

    Update VesiParserVehje.java

--- VesiParserVehje.java
+++ VesiParserVehje.java
@@ -1,81 +1,65 @@
 package com.example.harkkaduuni;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 
-public class VesiParserVehje {
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+public class TuuliParserVehje {
+
     //Singleton:
 
-    private static final VesiParserVehje P = new VesiParserVehje();
+    private static final TuuliParserVehje P = new TuuliParserVehje();
 
-    public static VesiParserVehje getInstance() {
+    public static TuuliParserVehje getInstance() {
         return P;
     }
 
-    private VesiParserVehje() {
+    private TuuliParserVehje(){
 
     }
 
-    public static ArrayList parseWater() {
+    // Tämä metodi Hakee käyttäjän syöttämän paikkakunnan ja senhetkisen kellonajan mukaan ilmatieteen avoimesta rajapinnasta säädataa.
+    // metodi käsittelee xml-muotoisen dokumentin ja palauttaa String-muotoisen tuulennopeuden.
+
+    public static ArrayList parseWind(){
         ArrayList tuotos = new ArrayList<>();
+        String sijainti = Fragment1.sijainti;
+        String paiva = Fragment1.paiva;
+        String aika = Fragment1.aika;
 
-        String json = getJSON();
-        System.out.println(json);
+            String url_tuuli = "https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::forecast::hirlam::surface::point::simple&place=" + sijainti + "&parameters=WindSpeedMS&timeStep=60&starttime=" + paiva + "T" + aika + "Z&endtime=" + paiva + "T" + aika + "Z";
 
-        if (json != null) {
-            try {
-                JSONObject a = new JSONObject(json);
-                JSONArray jsonarray = a.getJSONArray("value");
-                for (int i = 0; i < jsonarray.length(); i++) {
-                    JSONObject jobject = jsonarray.getJSONObject(i);
-                    System.out.println("++++++++++" + (i + 1) + "+++++++++");
-                    String jarvi = jobject.getString("Nimi");
-                    tuotos.add(jarvi);
-                    System.out.println(jarvi);
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
+        try {
+
+            DocumentBuilder docB = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document xmldoc = docB.parse(url_tuuli);
+
+            Element root = xmldoc.getDocumentElement();
+            root.normalize();
+            String windSpeed = root.getElementsByTagName("BsWfs:ParameterValue").item(0).getTextContent();
+            System.out.println(windSpeed);
+            tuotos.add(windSpeed);
         }
+        catch (ParserConfigurationException e) {
+            System.err.println("Couldn't initialize parser.");
+            e.printStackTrace();
+        }
+        catch (SAXException | IOException e) {
+            System.err.println("Couldn't fetch API data.");
+            e.printStackTrace();
+        }
+
 
         return tuotos;
     }
-
-    public static String getJSON() {
-        String sijainti = Fragment1.sijainti;
-        String response = null;
-        try {
-            URL vesiurl = new URL("http://rajapinnat.ymparisto.fi/api/jarvirajapinta/1.0/odata/Jarvi?$top=10&$filter=KuntaNimi%20eq%20%27"+sijainti+"%27");
-            HttpURLConnection conn = (HttpURLConnection) vesiurl.openConnection();
-            conn.setRequestMethod("GET");
-            InputStream in = new BufferedInputStream(conn.getInputStream());
-            BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            StringBuilder sb = new StringBuilder();
-            String line = null;
-            while ((line = br.readLine()) != null) {
-                sb.append(line).append("\n");
-            }
-            response = sb.toString();
-            in.close();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        ;
-        return response;
-    }
-
 
 }
